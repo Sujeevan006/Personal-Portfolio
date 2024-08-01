fb45899d61b79985551b2f0ce147a1a4a9727249
Sujeevan <avsinfo0824@gmail.com>
2024-08-01 12:08:28 +0530

    landing page - symbols removed

--- src/templates/home/landing/page.tsx
+++ src/templates/home/landing/page.tsx
@@ -8,16 +8,16 @@ export default function Landing() {
       <div className="flex flex-col lg:flex-row w-full py-10 items-center">
         <div className="flex flex-col lg:w-2/3 justify-center items-center">
           <div className="space-y-4">
-            <h1 className="text-[3vh]">Hello Nanpargale !</h1>
+            <h1 className="text-[3vh]">Hello Nanpargale</h1>
             <h2 className="text-[5vh]">I am Amirthalingam Sujeevan</h2>
             <h3>UI / UX Engineer </h3>
             <p className="text-justify">
-              I'm a person who has a keen interest in the design layout. I think
-              presenting an attractive design is a matter of concern in
-              developing a branding of products. To creates a good design, I
+              I am a person who has a keen interest in the design layout. I
+              think presenting an attractive design is a matter of concern in
+              developing a branding of products. To creates a good design. I
               focus on proper composition and visual decoration details in order
-              to make it more professional. For the time being, I'm developing
-              the skill for acquiring the UI/UX design for dynamic application
+              to make it more professional. For the time being. I am developing
+              the skill for acquiring the UI / UX design for dynamic application
               and web development.
             </p>
           </div>
