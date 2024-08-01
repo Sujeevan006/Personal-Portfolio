3d3a757a2f6597c5cd3abb877098d9368616081c
Sujeevan <avsinfo0824@gmail.com>
2024-08-01 12:07:45 +0530

    Main page modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
 import Header from "@/components/base/header";
 import Landing from "@/templates/home/landing/page";
+import Image from "next/image";
 import Projects from "@/templates/home/projects/page";
+import SocialMedia from "@/templates/home/socialmedias/page";
 
 export default function Home() {
   return (
     <>
       <div className="p-6 h-auto bg-gradient-to-r from-[#06140d] to-[#000c24]">
-        <Header />
-        <div className="lg:mx-[6%] md:mx-[2%]">
-          <Landing />
-          <Projects />
+        <div>
+          <Header />
+          <div className="lg:mx-[6%] md:mx-[2%] space-y-10">
+            <Landing />
+            <SocialMedia />
+            <Projects />
+          </div>
         </div>
       </div>
     </>
