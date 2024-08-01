7d64c3d4d5e5577e38d98b36341a398535a42cd3
Sujeevan <avsinfo0824@gmail.com>
2024-08-01 23:53:51 +0530

    Main home page modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,19 @@ import Landing from "@/templates/home/landing/page";
 import Image from "next/image";
 import Projects from "@/templates/home/projects/page";
 import SocialMedia from "@/templates/home/socialmedias/page";
+import Reviews from "@/templates/home/reviews/page";
 
 export default function Home() {
   return (
     <>
-      <div className="p-6 h-auto bg-gradient-to-r from-[#06140d] to-[#000c24]">
-        <div>
+      <div className="p-2 px-6 h-auto bg-gradient-to-r from-[#06140d] to-[#000c24]">
+        <div className="">
           <Header />
-          <div className="lg:mx-[6%] md:mx-[2%] space-y-10">
+          <div className="mx-[10%] space-y-10">
             <Landing />
             <SocialMedia />
             <Projects />
+            <Reviews />
           </div>
         </div>
       </div>
