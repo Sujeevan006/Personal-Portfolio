ed59582d8d53a351b6382398e96ce7bdd417ae51
Sujeevan <avsinfo0824@gmail.com>
2025-05-24 17:55:34 +0530

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
-import Header from "@/components/base/header";
-import Landing from "@/templates/home/landing/page";
-import Image from "next/image";
-import Projects from "@/templates/home/projects/page";
-import SocialMedia from "@/templates/home/socialmedias/page";
-import Reviews from "@/templates/home/reviews/page";
+import Header from '@/components/base/header';
+import Landing from '@/templates/home/landing/page';
+import Image from 'next/image';
+import Projects from '@/templates/home/projects/page';
+import SocialMedia from '@/templates/home/socialmedias/page';
+import Reviews from '@/templates/home/reviews/page';
+import Footer from '@/components/base/footer';
 
 export default function Home() {
   return (
     <>
-      <div className="p-2 px-6 h-auto bg-gradient-to-r from-[#06140d] to-[#000c24]">
-        <div className="">
+      <div className='p-2 px-6 h-auto bg-gradient-to-r from-[#06140d] to-[#000c24]'>
+        <div className=''>
           <Header />
-          <div className="mx-[10%] space-y-10">
+          <div className='mx-[10%] space-y-10'>
             <Landing />
             <SocialMedia />
             <Projects />
             <Reviews />
+            <Footer />
           </div>
         </div>
       </div>
---
+++ src/components/base/footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '../../shared/button';
+import footerImg from '../../../assets/images/footer.svg';
+
+export default function Footer() {
+  return (
+    <>
+      <div className='w-full'>
+        <img src='footerImg' alt='Footer Image' />
+      </div>
+    </>
+  );
+}
